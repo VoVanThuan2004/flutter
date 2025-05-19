8d77f4d37d768abfb3e2b54fc1717bc5204db2ce
VoVanThuan2004 <vot04766@gmail.com>
2025-05-19 13:03:31 +0700

    Update product

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1791851659"
+    serviceWorkerVersion: "362593895"
   }
 });
